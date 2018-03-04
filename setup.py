44acafb6a98a5111ea3ccdc1be154b3ed19167f3
namuyan <thhjuu@yahoo.co.jp>
2018-03-04 20:52:12 +0900

    Fix: Licence class

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ setup(
     author='namuyan',
     url='https://github.com/namuyan/bin-json',
     py_modules=['bjson'],
-    license="MIT"
+    license="MIT Licence",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
